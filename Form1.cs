a507b289aa50e29ac41ad723626f0f9bd89bd9c9
Gennadiy <genakisil@gmail.com>
2019-10-11 17:21:26 +0300

    Create

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Displays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("List of displays:");
+            foreach (Screen scr in Screen.AllScreens)
+            {
+                listBox1.Items.Add("Name" + scr.DeviceName);
+                listBox1.Items.Add("Bounds" + scr.Bounds.ToString());
+                listBox1.Items.Add("Working area" + scr.WorkingArea.ToString());
+                listBox1.Items.Add("Primary screen" + scr.Primary.ToString());
+            }
+        }
+    }
+}
